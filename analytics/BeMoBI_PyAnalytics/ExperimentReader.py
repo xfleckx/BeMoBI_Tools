c4ff81ee44580c3316e6774c0816d92f5db08033
Markus Fleck <fleckmarkus.privat@gmail.com>
2016-05-23 00:37:18 +0200

    [WIP] Add approach to read experiment data

---
+++ analytics/BeMoBI_PyAnalytics/ExperimentReader.py
@@ -0,0 +1,12 @@
+
+
+class ExperimentReader(object):
+    """description of class"""
+
+
+    def __init__(self, **kwargs):
+       with open( kwargs['fileName'] ) as streamSource:
+           for line in streamSource:
+               pass
+
+        
